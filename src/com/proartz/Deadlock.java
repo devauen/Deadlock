c6ebc6f3d5c6f70824c9bcd8dacd18420c5e7c65
Marcin Filipowski <proartz.devon@gmail.com>
2019-02-19 16:24:41 +0100

    Use Lock objects

--- src/com/proartz/Deadlock.java
+++ src/com/proartz/Deadlock.java
@@ -1,20 +1,77 @@
 package com.proartz;
 
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Deadlock {
     static class Friend{
         private final String name;
+        private final Lock lock = new ReentrantLock();
+
         public Friend(String name){
             this.name = name;
         }
         public String getName(){
             return this.name;
         }
-        public synchronized void bow(Friend bower){
-            System.out.format("%s: %s bow to me%n", this.getName(), bower.getName());
-            bower.bowBack(this);
+        public boolean impendingBow(Friend bower){
+            Boolean myLock = false;
+            Boolean yourLock = false;
+            try{
+                myLock = lock.tryLock();
+                yourLock = bower.lock.tryLock();
+            } finally {
+                if(! (myLock && yourLock)){
+                    if(myLock) {
+                        lock.unlock();
+                    }
+                    if(yourLock) {
+                        bower.lock.unlock();
+                    }
+                }
+            }
+            return myLock && yourLock;
         }
-        public synchronized void bowBack(Friend bower){
+
+        public void bow(Friend bower){
+            if(impendingBow(bower)){
+                try {
+                    System.out.format("%s: %s has bow to me!%n",
+                            this.getName(), bower.getName());
+                    bower.bowBack(this);
+                } finally {
+                    lock.unlock();
+                    bower.lock.unlock();
+                }
+            } else {
+                System.out.format("%s: %s started to bow to me, " +
+                        "but saw i was already bowing to him.%n", this.getName(), bower.getName());
+            }
+
+        }
+        public void bowBack(Friend bower){
             System.out.format("%s: %s has bowedback to me%n", this.getName(), bower.getName());
+        }
+    }
+
+    static class BowLoop implements Runnable{
+        private Friend bower;
+        private Friend bowee;
+
+        public BowLoop(Friend bower, Friend bowee) {
+            this.bower = bower;
+            this.bowee = bowee;
+        }
+
+        public void run() {
+            Random random = new Random();
+            for(;;) {
+                try {
+                    Thread.sleep(random.nextInt(1000));
+                } catch (InterruptedException e) {}
+                bowee.bow(bower);
+            }
         }
     }
 
@@ -22,12 +79,7 @@ public class Deadlock {
         final Friend alfonso = new Friend("Alfonso");
         final Friend gatsby = new Friend("Gatsby");
 
-        new Thread(new Runnable() {
-            public void run() { alfonso.bow(gatsby); }
-        }).start();
-
-        new Thread(new Runnable() {
-            public void run() { gatsby.bow(alfonso); }
-        }).start();
+        new Thread(new BowLoop(alfonso, gatsby)).start();
+        new Thread(new BowLoop(gatsby, alfonso)).start();
     }
 }
